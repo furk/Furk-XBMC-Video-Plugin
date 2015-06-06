fabaaea24d8bf6285748eeea5d906068e2ef9949
Gpun Yog <admin@furk.net>
2015-06-06 08:04:13 +0000

    api url host changed to www.furk.net and code linting

--- default.py
+++ default.py
@@ -17,20 +17,23 @@
 '''
 
 
-
 import sys
-import xbmc, xbmcaddon, xbmcgui, xbmcplugin
-import urllib, urllib2
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
+import urllib
+import urllib2
 import re
 
 # quircks for old xbmc
 try:
-	import json
-	from urlparse import parse_qs
-except: 
-	# pre-frodo and python 2.4
-	import simplejson as json
-	from cgi import parse_qs
+    import json
+    from urlparse import parse_qs
+except:
+    # pre-frodo and python 2.4
+    import simplejson as json
+    from cgi import parse_qs
 
 # Plugin constants
 __settings__ = xbmcaddon.Addon(id='plugin.video.furk')
@@ -40,36 +43,36 @@ __url__ = 'https://www.furk.net/t/xbmc'
 __version__ = '1.0.11'
 print "[PLUGIN] '%s: version %s' initialized! argv=%s" % (__plugin__, __version__, sys.argv)
 
-API_URL = "https://api.furk.net/api/"
+API_URL = "https://www.furk.net/api/"
 
 pluginhandle = None
 try:
-    pluginhandle = int( sys.argv[ 1 ] )
+    pluginhandle = int(sys.argv[1])
 except:
     pluginhandle = ""
 
 
-
 # based on whatthefurk plugin code
 class FurkAPI(object):
+
     def __init__(self, api_key=''):
         self.api_key = api_key
-        
+
     def metasearch(self, params):
         params['type'] = 'video'
         params['filter'] = 'cached'
         resp = self._call('plugins/metasearch', params)
-        if resp.has_key('files'):
+        if 'files' in resp:
             return resp['files']
         else:
             return None
-        
+
     def file_get(self, params={}):
         params['type'] = 'video'
         resp = self._call('file/get', params)
         files = resp['files']
         return files
-        
+
     def login(self, login, pwd):
         resp = self._call('login/login', {"login": login, "pwd": pwd})
         self.api_key = resp['api_key']
@@ -78,7 +81,7 @@ class FurkAPI(object):
     def _call(self, cmd, params):
         url = "%s%s" % (API_URL, cmd)
         body = self._fetch(url, params)
-        
+
         data = json.loads(body)
         if data['status'] != 'ok':
             xbmcgui.Dialog().ok('Error', data['error'])
@@ -104,34 +107,43 @@ def add_pseudo_items():
     url = sys.argv[0] + '?action=search&query='
     listitem = xbmcgui.ListItem(name)
     listitem.setInfo(type='video', infoLabels={'Title': name})
-    xbmcplugin.addDirectoryItem(handle=pluginhandle, url=url, listitem=listitem, isFolder=True)
+    xbmcplugin.addDirectoryItem(
+        handle=pluginhandle,
+        url=url,
+        listitem=listitem,
+        isFolder=True)
 
     if __settings__.getSetting('recent_queries') != '':
         name = '[SEARCH HISTORY]'
         url = sys.argv[0] + '?action=search_history'
         listitem = xbmcgui.ListItem(name)
         listitem.setInfo(type='video', infoLabels={'Title': name})
-        xbmcplugin.addDirectoryItem(handle=pluginhandle, url=url, listitem=listitem, isFolder=True)
-
+        xbmcplugin.addDirectoryItem(
+            handle=pluginhandle,
+            url=url,
+            listitem=listitem,
+            isFolder=True)
 
 
 def runner():
     # check if login and pass has been set
     if __settings__.getSetting('api_key') == '' or (__settings__.getSetting('login') == '' or __settings__.getSetting('password') == ''):
-        resp = xbmcgui.Dialog().yesno("No username/password set!","Furk.net requires you to be logged in to view", \
+        resp = xbmcgui.Dialog().yesno(
+            "No username/password set!",
+            "Furk.net requires you to be logged in to view",
             "videos.  Would you like to log-in now?")
         if resp:
             __settings__.openSettings()
         else:
             return
-    
+
     # get params
     params = parse_qs(sys.argv[2][1:])
     for k in params:
         params[k] = params[k][0]
     xbmc.log("params=%s" % params)
 
-    # show files list by default 
+    # show files list by default
     if not params:
         params['action'] = 'root'
 
@@ -140,16 +152,17 @@ def runner():
     if(params['action'] == 'play'):
         xbmc.Player().play(urllib.unquote(params['url']))
         return
-    
+
     # api obj is needed
     api_key = __settings__.getSetting('api_key')
     if api_key == '':
         api = FurkAPI()
-        api_key = api.login(login=__settings__.getSetting('login'), pwd=__settings__.getSetting('password'))
+        api_key = api.login(
+            login=__settings__.getSetting('login'),
+            pwd=__settings__.getSetting('password'))
         __settings__.setSetting('api_key', api_key)
     else:
         api = FurkAPI(api_key)
-
 
     # root menu
     if(params['action'] == 'root'):
@@ -160,11 +173,20 @@ def runner():
         for fl in files:
             url = sys.argv[0] + '?action=file&id=' + fl['id']
             listitem = xbmcgui.ListItem(fl['name'])
-            if fl.has_key('ss_urls_tn_all'):
-                listitem.setThumbnailImage(fl['ss_urls_tn_all']);
-            listitem.setInfo(type='video', infoLabels={'title': fl['name'], 'size': int(fl['size'])})
-            xbmcplugin.addDirectoryItem(handle=pluginhandle, url=url, listitem=listitem, isFolder=True)
-        
+            if 'ss_urls_tn_all' in fl:
+                listitem.setThumbnailImage(fl['ss_urls_tn_all'])
+            listitem.setInfo(
+                type='video',
+                infoLabels={
+                    'title': fl['name'],
+                    'size': int(
+                        fl['size'])})
+            xbmcplugin.addDirectoryItem(
+                handle=pluginhandle,
+                url=url,
+                listitem=listitem,
+                isFolder=True)
+
         xbmcplugin.endOfDirectory(pluginhandle)
 
     elif(params['action'] == 'file'):
@@ -175,14 +197,21 @@ def runner():
                 continue
             url = sys.argv[0] + '?action=play&url=' + item['url_dl']
             name = item['name']
-            if item.has_key('bitrate'):
+            if 'bitrate' in item:
                 name = '%s %skb/s' % (item['name'], item['bitrate'])
             listitem = xbmcgui.ListItem(name)
-            if item.has_key('url_tn'):
-                listitem.setThumbnailImage(item['url_tn']);
-            listitem.setInfo('video', {'title': name, 'size': int(item['size']), 'duration': item['length']})
-            xbmcplugin.addDirectoryItem(handle=pluginhandle, url=url, listitem=listitem, isFolder=True)
-        
+            if 'url_tn' in item:
+                listitem.setThumbnailImage(item['url_tn'])
+            listitem.setInfo(
+                'video', {
+                    'title': name, 'size': int(
+                        item['size']), 'duration': item['length']})
+            xbmcplugin.addDirectoryItem(
+                handle=pluginhandle,
+                url=url,
+                listitem=listitem,
+                isFolder=True)
+
         xbmcplugin.endOfDirectory(pluginhandle)
 
     elif(params['action'] == 'search_history'):
@@ -199,15 +228,19 @@ def runner():
 
             listitem = xbmcgui.ListItem(name)
             listitem.setInfo(type='video', infoLabels={'Title': name})
-            xbmcplugin.addDirectoryItem(handle=pluginhandle, url=url, listitem=listitem, isFolder=True)
+            xbmcplugin.addDirectoryItem(
+                handle=pluginhandle,
+                url=url,
+                listitem=listitem,
+                isFolder=True)
         xbmcplugin.endOfDirectory(pluginhandle)
 
     elif(params['action'] == 'search'):
-        if not params.has_key('q'):
+        if 'q' not in params:
             params['q'] = ''
         keyboard = xbmc.Keyboard(urllib.unquote(params['q']), 'Search')
         keyboard.doModal()
-        
+
         if not keyboard.isConfirmed():
             print 'not confirmed'
             return
@@ -220,22 +253,31 @@ def runner():
 
         # add history
         recent = __settings__.getSetting('recent_queries').split('|')
-        recent = [ urllib.unquote(r) for r in recent ]
+        recent = [urllib.unquote(r) for r in recent]
         if params['q'] in recent:
             recent.remove(params['q'])
         recent.insert(0, params['q'])
-        recent = [ urllib.quote(r) for r in recent ]
+        recent = [urllib.quote(r) for r in recent]
         __settings__.setSetting(id='recent_queries', value='|'.join(recent))
 
         add_pseudo_items()
         for item in files:
             url = sys.argv[0] + '?action=file&id=' + item['id']
             listitem = xbmcgui.ListItem(item['name'])
-            if item.has_key('ss_urls_tn_all'):
-                listitem.setThumbnailImage(item['ss_urls_tn_all']);
-            listitem.setInfo(type='video', infoLabels={'title': item['name'], 'size': int(item['size'])})
-            xbmcplugin.addDirectoryItem(handle=pluginhandle, url=url, listitem=listitem, isFolder=True)
-        
+            if 'ss_urls_tn_all' in item:
+                listitem.setThumbnailImage(item['ss_urls_tn_all'])
+            listitem.setInfo(
+                type='video',
+                infoLabels={
+                    'title': item['name'],
+                    'size': int(
+                        item['size'])})
+            xbmcplugin.addDirectoryItem(
+                handle=pluginhandle,
+                url=url,
+                listitem=listitem,
+                isFolder=True)
+
         xbmcplugin.endOfDirectory(pluginhandle)
 
     elif(params['action'] == 'search_history'):
@@ -250,18 +292,17 @@ def runner():
 
             listitem = xbmcgui.ListItem(name)
             listitem.setInfo(type='video', infoLabels={'Title': name})
-            xbmcplugin.addDirectoryItem(handle=pluginhandle, url=url, listitem=listitem, isFolder=True)
+            xbmcplugin.addDirectoryItem(
+                handle=pluginhandle,
+                url=url,
+                listitem=listitem,
+                isFolder=True)
 
         xbmcplugin.endOfDirectory(pluginhandle)
 
 
-
 if __name__ == "__main__":
     runner()
-            
 
 
 sys.modules.clear()
-
-
-
